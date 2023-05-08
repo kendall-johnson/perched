1a18a40e51f6ca83f1b5d38084ab8a5acd032097
kendall-johnson <kdotjohnson2019@gmail.com>
2023-05-07 20:49:24 -0700

    Switch User Authentication to MongoDB and Token based, set up express and node back end

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import { login } from '../../utilities/users-service';
+// import { useNavigate } from 'react-router-dom';
+
+export default function LoginForm({ setUser, showLogin, setShowLogin }) {
+// const navigate = useNavigate();
+const [credentials, setCredentials] = useState({
+email: '',
+password: ''
+});
+const [error, setError] = useState('');
+
+function handleChange(evt) {
+setCredentials({ ...credentials, [evt.target.name]: evt.target.value });
+setError('');
+}
+
+async function handleSubmit(evt) {
+evt.preventDefault();
+try {
+const user = await login(credentials);
+setUser(user);
+} catch {
+setError('Log In Failed - Try Again');
+}
+}
+
+  return (
+    <>
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+    <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
+            WELCOME BACK
+        </h1>
+        <form className="mt-6" onSubmit={handleSubmit}>
+            <div className="mb-2">
+                <label
+                    
+                    className="block text-sm font-semibold text-gray-800"
+                >
+                    Email
+                </label>
+                <input
+                    type="email"
+                    name="email"
+                    className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    value={credentials.email} 
+                    onChange={handleChange} required
+                />
+            </div>
+            <div className="mb-2">
+                <label
+                    
+                    className="block text-sm font-semibold text-gray-800"
+                >
+                    Password
+                </label>
+                <input
+                    type="password"
+                    className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    name="password" 
+                    value={credentials.password} 
+                    onChange={handleChange} 
+                    required
+                />
+            </div>
+            <div className="mt-6">
+                <button  type="submit" className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                    Login
+                </button>
+            </div>
+        </form>
+        
+
+        <p className="mt-8 text-xs font-light text-center text-gray-700">
+            {" "}
+            Don't have an account?{" "}
+            <a
+                className="font-medium text-purple-600 hover:underline"
+                onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}
+            
+               
+            </a>
+        </p>
+        {error && <p className="error-message pt-12">&nbsp;{error}</p>}
+    </div>
+</div>
+</>
+  );
+}
---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import { signUp } from '../../utilities/users-service';
+import { useNavigate } from 'react-router-dom';
+
+export default function SignUpForm({  setUser,  showLogin, setShowLogin }) {
+  // const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirm: '',
+    error: '',
+  });
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    try {
+      const { error, confirm, ...data } = formData;
+      const user = await signUp(data);
+      setUser(user);
+      // navigate('/home');
+    } catch {
+      setFormData({ ...formData, error: 'Sign Up Failed - Try Again' });
+    }
+  };
+
+  const handleChange = (evt) => {
+    setFormData({
+      ...formData,
+      [evt.target.name]: evt.target.value,
+      error: '',
+    });
+  };
+
+  const disable = formData.password !== formData.confirm;
+
+  return (
+    <>
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+    <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
+    <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
+      JOIN BRAINWAVES
+    </h1>
+    <form className="mt-6" onSubmit={handleSubmit}>
+    <div className="mb-2">
+        <label
+          htmlFor="email"
+          className="block text-sm font-semibold text-gray-800"
+        >
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mb-2">
+        <label
+          htmlFor="email"
+          className="block text-sm font-semibold text-gray-800"
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mb-2">
+        <label
+          htmlFor="password"
+          className="block text-sm font-semibold text-gray-800"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mb-2">
+        <label
+          htmlFor="password_confirmation"
+          className="block text-sm font-semibold text-gray-800"
+        >
+          Confirm password
+        </label>
+        <input
+          type="password"
+          className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          name="confirm"
+          value={formData.confirm}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mt-6">
+        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+          Sign up
+        </button>
+      </div>
+    </form>
+    <p className="mt-8 text-xs font-light text-center text-gray-700">
+        {" "}
+        Already have an account?{" "}
+        <a
+            className="font-medium text-purple-600 hover:underline"
+            onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}
+        </a>
+    </p>
+</div>
+</div>
+</>
+  );
+}
+
+
